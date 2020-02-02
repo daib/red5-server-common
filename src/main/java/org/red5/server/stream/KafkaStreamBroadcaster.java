c616dbe0928f1b28e410a95dd44ff110e4054b7f
Dai Bui <dbui@confluent.io>
2020-02-02 01:37:06 -0800

    Add manage resource annotation for KafkaStreamBroadcaster.

--- src/main/java/org/red5/server/stream/KafkaStreamBroadcaster.java
+++ src/main/java/org/red5/server/stream/KafkaStreamBroadcaster.java
@@ -2,10 +2,12 @@ package org.red5.server.stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 /**
  * This class reads from Kafka and broadcasts to subscribers
  */
+@ManagedResource(objectName = "org.red5.server:type=KafkaStreamBroadcaster", description = "KafkaStreamBroadcaster")
 public class KafkaStreamBroadcaster extends ClientBroadcastStream {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaStreamBroadcaster.class);
