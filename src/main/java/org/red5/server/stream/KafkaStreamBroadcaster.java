14fc3090f367c08ef702d311711b97b26f7ab2b2
Dai Bui <dbui@confluent.io>
2020-02-09 11:23:56 -0800

    Enable broadcast from kafka

--- src/main/java/org/red5/server/stream/KafkaStreamBroadcaster.java
+++ src/main/java/org/red5/server/stream/KafkaStreamBroadcaster.java
@@ -1,5 +1,7 @@
 package org.red5.server.stream;
 
+import org.red5.server.messaging.IPipe;
+import org.red5.server.messaging.PipeConnectionEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jmx.export.annotation.ManagedResource;
@@ -12,6 +14,10 @@ public class KafkaStreamBroadcaster extends ClientBroadcastStream {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaStreamBroadcaster.class);
 
+    protected transient IPipe cachedLivePipe = null;
+
+    protected boolean broadcastFromKafka = true;
+
     protected String kafkaBrokerAddress;
 
     /**
@@ -19,7 +25,17 @@ public class KafkaStreamBroadcaster extends ClientBroadcastStream {
      *            the kafkaBrokerAddress to set
      */
     public void setKafkaBrokerAddress(String kafkaBrokerAddress) {
+        log.info("Setting kafka address {}", kafkaBrokerAddress);
         this.kafkaBrokerAddress = kafkaBrokerAddress;
+    }
+
+    /**
+     * @param broadcastFromKafka
+     *            the broadcastFromKafka to set
+     */
+
+    public void setBroadcastFromKafka(boolean broadcastFromKafka) {
+        this.broadcastFromKafka = broadcastFromKafka;
     }
 
     public KafkaStreamBroadcaster() {
@@ -31,4 +47,23 @@ public class KafkaStreamBroadcaster extends ClientBroadcastStream {
         //TODO: init the consumer and the thread to poll from Kafka and write to livePipe
     }
 
+    @Override
+    public void onPipeConnectionEvent(PipeConnectionEvent event) {
+        super.onPipeConnectionEvent(event);
+
+        // if we broadcast from Kafka stream, set the livePipe to null to disable
+        // direct live broadcast within function dispatch
+        if (broadcastFromKafka && livePipe != null) {
+            cachedLivePipe = livePipe;
+        }
+    }
+
+    @Override
+    public void close() {
+        if (broadcastFromKafka && cachedLivePipe != null) {
+            livePipe = cachedLivePipe;
+            cachedLivePipe = null;
+        }
+        super.close();
+    }
 }
